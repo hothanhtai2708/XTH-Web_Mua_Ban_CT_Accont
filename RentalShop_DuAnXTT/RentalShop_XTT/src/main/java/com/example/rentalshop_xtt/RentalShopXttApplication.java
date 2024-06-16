92dd5a567e3ab20c53491c1eea192c93172b99e3
Trongle0511 <trongle311021@gmail.com>
2024-06-16 15:17:21 +0700

    update_maven_DuAn

---
+++ RentalShop_DuAnXTT/RentalShop_XTT/src/main/java/com/example/rentalshop_xtt/RentalShopXttApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentalshop_xtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalShopXttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalShopXttApplication.class, args);
+	}
+
+}
